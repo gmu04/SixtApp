ee2cdbe084da164beacaee5d1bafef4164c58566
GokhanMutlu <gokhanmutlu04@yahoo.com>
2021-07-04 08:58:56 +0300

    Initial Commit

---
+++ SixtApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SixtApp
+//
+//  Created by Gokhan Mutlu on 4.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
