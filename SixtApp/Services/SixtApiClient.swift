bb43b7e8c805a14fdf11b4a017013e3a61dc4b22
GokhanMutlu <gokhanmutlu04@yahoo.com>
2021-07-04 17:31:05 +0300

    SixtApiClient implemented - it is fetching & parsing json data

--- SixtApp/Services/SixtApiClient.swift
+++ SixtApp/Services/SixtApiClient.swift
@@ -9,12 +9,14 @@ import Foundation
 
 /// Sixt API client which manages all API calls
 ///
-final class SixtApiClient: ApiClient{
+final class SixtApiClient: NSObject, ApiClient, URLSessionDelegate{
     
     
     //MARK: - Properties
-    
+    private(set) var baseURL: URL!
 
+    
+    
     /*  Comment for reviewer:
         I did not prefer to use singleton pattern because I am injecting api client when the app stats, and no need to create it again.
         Besides SixtApi is just used to fetch data - Any threading issues expected for current scenario(s)
@@ -24,20 +26,53 @@ final class SixtApiClient: ApiClient{
         guard let url = URL(string: baseUrl) else{
             return nil
         }
-        super.init()
-        self.setBaseURL(url)
+        self.baseURL = url
     }
 
+    
+    
+    
+    func fetch(path:String, completion: @escaping (Result<Any, ApiError>)->Void){
+        guard let validURL = URL(string: path, relativeTo: self.baseURL) else {
+            completion(.failure(ApiError.cannotInstantiateUrl("\(baseURL.absoluteString) + path: \(path)")))
+            return
+        }
+        //print("\n GET: \(validURL.absoluteString)\n")
+        
+        let urlSession = URLSession(configuration: URLSessionConfiguration.default, delegate: self, delegateQueue: .main)
+        urlSession.dataTask(with: validURL) { (data, response, error) in
+            
+            //check data & error
+            guard let validData = data, error == nil else {
+                //print("\n -> ERROR= \(error!.localizedDescription)\n")
+                completion(.failure(ApiError.error(error!)))
+                return
+            }
+            
+            //Comment for reviewer: if error is nil we do not need check it further when fetching data.
+            //  But other situations (ex: CRUD operations) it is mandatory. It is good practice to write following snippet
+            
+            //check if any http error
+            if let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode != 200 {
+                //print("API call status: http\(httpURLResponse.statusCode), \"\(httpURLResponse.description)\"")
+                completion(.failure(ApiError.httpError(httpURLResponse.statusCode, httpURLResponse.description)))
+                return
+            }
+            
 
-    override func fetch(path: String, completion: @escaping (Result<Any, ApiError>) -> Void) {
-        super.fetch(path: path) { result  in //Result<Any, ApiError>
             
             //parse json here
+            do {
+                let cars = try JSONDecoder().decode([Car].self, from: validData)
+                //print(cars)
+                completion(.success(cars))
+            } catch {
+                print(error.localizedDescription, error)
+                completion(.failure(.serializationError(error)))
+            }
             
             
-        }
+        }.resume()
     }
-    
-    
-    
+
 }
